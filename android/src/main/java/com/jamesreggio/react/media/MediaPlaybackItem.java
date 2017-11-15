e59488d20a3ce05c41fad9118f354a4df56f693c
James Reggio <james.reggio@gmail.com>
2017-11-15 15:19:34 -0500

    Initial Android implementation

---
+++ android/src/main/java/com/jamesreggio/react/media/MediaPlaybackItem.java
@@ -0,0 +1,379 @@
+package com.jamesreggio.react.media;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Build;
+
+import com.facebook.infer.annotation.Assertions;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MediaPlaybackItem {
+
+  public final int DEFAULT_UPDATE_INTERVAL = 30000;
+  public final int BOUNDARY_UPDATE_INTERVAL = 1000;
+
+  private final Integer key;
+  private final MediaPlaybackManager manager;
+
+  private MediaPlayer player;
+  private boolean buffering;
+  private OnPreparedListener preparedListener;
+  private OnSeekCompleteListener seekCompleteListener;
+
+  private Timer intervalTimer;
+  private TimerTask intervalTask;
+  private Integer updateInterval;
+
+  private Timer boundaryTimer;
+  private TimerTask boundaryTask;
+  private ReadableArray updateBoundaries;
+
+  /**
+   *
+   */
+
+  public interface OnPreparedListener {
+    void onPrepared(Throwable error);
+  }
+
+  public interface OnSeekCompleteListener {
+    void onSeekComplete(boolean finished);
+  }
+
+  /**
+   *
+   */
+
+  public MediaPlaybackItem(
+    final Integer key,
+    final MediaPlaybackManager manager
+  ) {
+    this.key = key;
+    this.manager = manager;
+  }
+
+  public void destroy() {
+    if (this.intervalTimer != null) {
+      this.intervalTimer.cancel();
+      this.intervalTimer = null;
+    }
+
+    if (this.boundaryTimer != null) {
+      this.boundaryTimer.cancel();
+      this.boundaryTimer = null;
+    }
+
+    if (this.player != null) {
+      this.player.release();
+      this.player = null;
+    }
+  }
+
+  /**
+   *
+   */
+
+  private void sendUpdate() {
+    this.sendUpdate(this.getStatus());
+  }
+
+  private void sendUpdate(String status) {
+    final WritableMap body = Arguments.createMap();
+    body.putInt("key", this.key);
+    body.putInt("position", this.getPosition());
+    body.putString("status", status);
+    this.manager.emit("updated", body);
+  }
+
+  private TimerTask getIntervalTask() {
+    final MediaPlaybackItem item = this;
+
+    return new TimerTask() {
+      @Override
+      public void run() {
+        item.sendUpdate();
+      }
+    };
+  }
+
+  private TimerTask getBoundaryTask() {
+    final MediaPlaybackItem item = this;
+
+    return new TimerTask() {
+      private Integer lastPosition;
+
+      @Override
+      public void run() {
+        final Integer position = item.getPosition();
+
+        if (lastPosition == null) {
+          lastPosition = position;
+          return;
+        }
+
+        int size = item.updateBoundaries.size();
+        for (int i = 0; i < size; i++) {
+          final Integer boundary = updateBoundaries.getInt(i);
+          if (
+            (boundary > lastPosition && boundary < position) ||
+            (boundary > position && boundary < lastPosition)
+          ) {
+            item.sendUpdate();
+            return;
+          }
+        }
+      }
+    };
+  }
+
+  //XXX adjust intervals for rate?
+  private void startTimers() {
+    if (this.intervalTask == null && this.intervalTimer != null) {
+      this.intervalTask = this.getIntervalTask();
+      this.intervalTimer.scheduleAtFixedRate(
+        this.intervalTask, 0, this.updateInterval
+      );
+    }
+
+    if (this.boundaryTask == null && this.boundaryTimer != null) {
+      this.boundaryTask = this.getBoundaryTask();
+      this.boundaryTimer.scheduleAtFixedRate(
+        this.boundaryTask, 0, BOUNDARY_UPDATE_INTERVAL
+      );
+    }
+  }
+
+  private void stopTimers() {
+    if (this.intervalTask != null) {
+      this.intervalTask.cancel();
+      this.intervalTask = null;
+    }
+
+    if (this.boundaryTask != null) {
+      this.boundaryTask.cancel();
+      this.boundaryTask = null;
+    }
+  }
+
+  /**
+   *
+   */
+
+  public void prepare(
+    final ReadableMap options,
+    final OnPreparedListener listener
+  ) {
+    Assertions.assertCondition(this.player == null, "Item already prepared");
+
+    try {
+      this.player = new MediaPlayer();
+      this.player.setAudioStreamType(AudioManager.STREAM_MUSIC); //XXX?
+      this.player.setDataSource(options.getString("url"));
+    } catch (IOException error) {
+      if (listener != null) {
+        listener.onPrepared(error);
+      }
+
+      this.player = null;
+      return;
+    }
+
+    final MediaPlaybackItem item = this;
+    this.preparedListener = listener;
+
+    this.player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+      @Override
+      public void onPrepared(final MediaPlayer player) {
+        if (item.preparedListener != null) {
+          item.preparedListener.onPrepared(null);
+          item.preparedListener = null;
+        }
+      }
+    });
+
+    this.player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+      @Override
+      public void onCompletion(final MediaPlayer player) {
+        item.sendUpdate("FINISHED");
+      }
+    });
+
+    this.player.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+      @Override
+      public boolean onError(
+        final MediaPlayer player,
+        final int what,
+        final int extra
+      ) {
+        //XXX pass what, extra
+
+        if (item.preparedListener != null) {
+          item.preparedListener.onPrepared(null);
+          item.preparedListener = null;
+        } else {
+          item.sendUpdate("FINISHED");
+        }
+
+        return true;
+      }
+    });
+
+    this.player.setOnInfoListener(new MediaPlayer.OnInfoListener() {
+      @Override
+      public boolean onInfo(
+        final MediaPlayer player,
+        final int what,
+        final int extra
+      ) {
+        if (what == MediaPlayer.MEDIA_INFO_BUFFERING_START) {
+          item.buffering = true;
+          item.sendUpdate();
+          return true;
+        }
+
+        if (what == MediaPlayer.MEDIA_INFO_BUFFERING_END) {
+          item.buffering = false;
+          item.sendUpdate();
+          return true;
+        }
+
+        return false;
+      }
+    });
+
+    this.player.setOnSeekCompleteListener(new MediaPlayer.OnSeekCompleteListener() {
+      @Override
+      public void onSeekComplete(final MediaPlayer player) {
+        if (item.seekCompleteListener == null) {
+          return;
+        }
+
+        item.seekCompleteListener.onSeekComplete(true);
+        item.seekCompleteListener = null;
+      }
+    });
+
+    this.player.prepareAsync();
+  }
+
+  public void activate(final ReadableMap options) {
+    Assertions.assertCondition(this.player != null, "Item not prepared");
+    Assertions.assertCondition(this.intervalTimer == null, "Item already activated");
+
+    if (!options.isNull("position")) {
+      this.seekTo(options.getInt("position"), null);
+    }
+
+    if (!options.isNull("rate")) {
+      this.setRate(options.getDouble("rate"));
+    }
+
+    this.intervalTimer = new Timer();
+
+    this.updateInterval = options.isNull("updateInterval")
+      ? DEFAULT_UPDATE_INTERVAL
+      : options.getInt("updateInterval");
+
+    if (!options.isNull("updateBoundaries")) {
+      this.updateBoundaries = options.getArray("updateBoundaries");
+      this.boundaryTimer = new Timer();
+    }
+  }
+
+  public void deactivate(final ReadableMap options) {
+    Assertions.assertCondition(this.player != null, "Item not prepared");
+    Assertions.assertCondition(this.intervalTimer != null, "Item not activated");
+    this.pause();
+
+    if (this.intervalTimer != null) {
+      this.intervalTimer.cancel();
+      this.intervalTimer = null;
+    }
+
+    if (this.boundaryTimer != null) {
+      this.boundaryTimer.cancel();
+      this.boundaryTimer = null;
+    }
+  }
+
+  public void release(final ReadableMap options) {
+    Assertions.assertCondition(this.player != null, "Item not prepared");
+
+    this.player.release();
+    this.player = null;
+  }
+
+  /**
+   * 
+   */
+
+  public void play() {
+    Assertions.assertNotNull(this.player).start();
+    this.startTimers();
+    // this.sendUpdate(); //XXX?
+  }
+
+  public void pause() {
+    Assertions.assertNotNull(this.player).pause();
+    this.stopTimers();
+    this.sendUpdate();
+  }
+
+  public void seekTo(
+    final Integer position,
+    final OnSeekCompleteListener listener
+  ) {
+    if (this.seekCompleteListener != null) {
+      this.seekCompleteListener.onSeekComplete(false);
+    }
+
+    this.seekCompleteListener = listener;
+    Assertions.assertNotNull(this.player).seekTo(position * 1000);
+  }
+
+  public void setRate(final Double rate) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      final MediaPlayer player = Assertions.assertNotNull(this.player);
+
+      player.setPlaybackParams(
+        player.getPlaybackParams().setSpeed(rate.floatValue())
+      );
+    }
+  }
+
+  public void setBuffer(final Integer duration) {
+    // Not possible on Android.
+  }
+
+  /**
+   *
+   */
+
+  public String getStatus() {
+    if (this.player == null) {
+      return "IDLE";
+    }
+
+    if (this.buffering) {
+      return "STALLED";
+    }
+
+    return this.player.isPlaying() ? "PLAYING" : "PAUSED";
+  }
+
+  public Integer getPosition() {
+    return Assertions.assertNotNull(this.player).getCurrentPosition() / 1000;
+  }
+
+  public Integer getDuration() {
+    return Assertions.assertNotNull(this.player).getDuration() / 1000;
+  }
+
+}
---
+++ android/src/main/java/com/jamesreggio/react/media/MediaPlaybackManager.java
@@ -0,0 +1,268 @@
+package com.jamesreggio.react.media;
+
+import com.facebook.infer.annotation.Assertions;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+
+import java.util.HashMap;
+
+import javax.annotation.Nullable;
+
+public class MediaPlaybackManager extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext context;
+  private final HashMap<Integer, MediaPlaybackItem> items = new HashMap<>();
+
+  /**
+   * Manager lifecycle.
+   */
+
+  public MediaPlaybackManager(final ReactApplicationContext context) {
+    super(context);
+    this.context = context;
+  }
+
+  public void destroy() {
+    for (final MediaPlaybackItem item : this.items.values()) {
+      item.destroy();
+    }
+
+    this.items.clear();
+  }
+
+  @Override
+  public String getName() {
+    return "MediaPlaybackManager";
+  }
+
+  /**
+   * Manager events.
+   */
+
+  private interface RCTDeviceEventEmitter extends JavaScriptModule {
+    void emit(String eventName, @Nullable Object data);
+  }
+
+  protected void emit(final String name) {
+    this.emit(name, null);
+  }
+
+  protected void emit(final String name, final Object body) {
+    this.context
+      .getJSModule(MediaPlaybackManager.RCTDeviceEventEmitter.class)
+      .emit(name, body);
+  }
+
+  /**
+   * Session lifecycle.
+   */
+
+  @ReactMethod
+  public void activateSession(
+    final Integer key,
+    final ReadableMap options,
+    final Promise promise
+  ) {
+    promise.resolve(null); //XXX
+  }
+
+  @ReactMethod
+  public void deactivateSession(
+    final Integer key,
+    final ReadableMap options,
+    final Promise promise
+  ) {
+    promise.resolve(null); //XXX
+  }
+
+  /**
+   * Item lifecycle.
+   */
+
+  @ReactMethod
+  public void prepareItem(
+    final Integer key,
+    final ReadableMap options,
+    final Promise promise
+  ) {
+    MediaPlaybackItem item = this.items.get(key);
+
+    if (item == null) {
+      item = new MediaPlaybackItem(key, this);
+      this.items.put(key, item);
+    }
+
+    final MediaPlaybackItem finalItem = item;
+
+    item.prepare(options, new MediaPlaybackItem.OnPreparedListener() {
+      boolean promiseResolved = false;
+
+      @Override
+      public void onPrepared(Throwable error) {
+        if (promiseResolved) {
+          return;
+        }
+
+        promiseResolved = true;
+
+        if (error != null) {
+          promise.reject(
+            "PLAYBACK_LOAD_FAILURE",
+            "The item failed to load",
+            error
+          );
+        } else {
+          WritableMap body = Arguments.createMap();
+          body.putInt("duration", finalItem.getDuration());
+          promise.resolve(body);
+        }
+      }
+    });
+  }
+
+  @ReactMethod
+  public void activateItem(
+    final Integer key,
+    final ReadableMap options,
+    final Promise promise
+  ) {
+    final MediaPlaybackItem item = this.items.get(key);
+    Assertions.assertNotNull(item).activate(options);
+    promise.resolve(null);
+  }
+
+  @ReactMethod
+  public void deactivateItem(
+    final Integer key,
+    final ReadableMap options,
+    final Promise promise
+  ) {
+    final MediaPlaybackItem item = this.items.get(key);
+    Assertions.assertNotNull(item).deactivate(options);
+    promise.resolve(null);
+  }
+
+  @ReactMethod
+  public void releaseItem(
+    final Integer key,
+    final ReadableMap options,
+    final Promise promise
+  ) {
+    final MediaPlaybackItem item = this.items.get(key);
+    Assertions.assertNotNull(item).release(options);
+    promise.resolve(null);
+  }
+
+  /**
+   * Item playback controls.
+   */
+
+  @ReactMethod
+  public void playItem(
+    final Integer key,
+    final Promise promise
+  ) {
+    final MediaPlaybackItem item = this.items.get(key);
+    Assertions.assertNotNull(item).play();
+    promise.resolve(null);
+  }
+
+  @ReactMethod
+  public void pauseItem(
+    final Integer key,
+    final Promise promise
+  ) {
+    final MediaPlaybackItem item = this.items.get(key);
+    Assertions.assertNotNull(item).pause();
+    promise.resolve(null);
+  }
+
+  @ReactMethod
+  public void seekItem(
+    final Integer key,
+    final Integer position,
+    final Promise promise
+  ) {
+    final MediaPlaybackItem item = this.items.get(key);
+
+    Assertions.assertNotNull(item).seekTo(
+      position,
+      new MediaPlaybackItem.OnSeekCompleteListener() {
+        boolean promiseResolved = false;
+
+        @Override
+        public void onSeekComplete(boolean finished) {
+          if (promiseResolved) {
+            return;
+          }
+
+          promiseResolved = true;
+          promise.resolve(finished);
+        }
+      }
+    );
+  }
+
+  @ReactMethod
+  public void setRateForItem(
+    final Integer key,
+    final Double rate,
+    final Promise promise
+  ) {
+    final MediaPlaybackItem item = this.items.get(key);
+    Assertions.assertNotNull(item).setRate(rate);
+    promise.resolve(null);
+  }
+
+  @ReactMethod
+  public void setBufferForItem(
+    final Integer key,
+    final Integer duration,
+    final Promise promise
+  ) {
+    final MediaPlaybackItem item = this.items.get(key);
+    Assertions.assertNotNull(item).setBuffer(duration);
+    promise.resolve(null);
+  }
+
+  /**
+   * Item playback properties.
+   */
+
+  @ReactMethod
+  public void getStatusForItem(
+    final Integer key,
+    final Promise promise
+  ) {
+    final MediaPlaybackItem item = this.items.get(key);
+    final String status = Assertions.assertNotNull(item).getStatus();
+    promise.resolve(status);
+  }
+
+  @ReactMethod
+  public void getPositionForItem(
+    final Integer key,
+    final Promise promise
+  ) {
+    final MediaPlaybackItem item = this.items.get(key);
+    final Integer position = Assertions.assertNotNull(item).getPosition();
+    promise.resolve(position);
+  }
+
+  @ReactMethod
+  public void getDurationForItem(
+    final Integer key,
+    final Promise promise
+  ) {
+    final MediaPlaybackItem item = this.items.get(key);
+    final Integer duration = Assertions.assertNotNull(item).getDuration();
+    promise.resolve(duration);
+  }
+
+}
